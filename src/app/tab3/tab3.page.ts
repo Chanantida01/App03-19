47d215961242c02be040ed9b5e597d440a528f13
Chanantida Krungmee <Cntdkm.kim@gmail.com>
2024-08-14 13:00:07 +0700

    first commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonLabel, IonItem, IonButton, IonModal, IonButtons, IonAvatar, IonImg, IonList, IonCheckbox, IonSearchbar} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { ActionSheetController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonLabel, IonItem, IonButton, IonModal, IonButtons, IonAvatar, IonImg, IonList, IonCheckbox, IonSearchbar]
 })
 export class Tab3Page {
-  constructor() {}
+  presentingElement = undefined;
+  constructor(private actionSheetCtrl: ActionSheetController) {}
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+
+
+  canDismiss = async () => {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Are you sure?',
+      buttons: [
+        {
+          text: 'Yes',
+          role: 'confirm',
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    actionSheet.present();
+
+    const { role } = await actionSheet.onWillDismiss();
+
+    return role === 'confirm';
+  };
 }
